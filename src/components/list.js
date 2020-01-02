af84214b072332b797ee7205afd2183d125da7f0
M'BOUKOU MONEKENE Aymard Ricardo <aymardmb@gmail.com>
2020-01-03 00:19:50 +0100

    remise à zéro du projet

--- src/components/list.js
+++ src/components/list.js
@@ -1,7 +1,7 @@
 "use-strict";
 
 import React from 'react';
-import {Container, Box, Heading} from 'gestalt';
+// import {Container, Box, Heading} from 'gestalt';
 
 class List extends React.Component {
 
@@ -15,76 +15,25 @@ class List extends React.Component {
 
         // vos bind etc...
         this.toggleChange = this.toggleChange.bind(this);
-        this.addTask = this.addTask.bind(this);
+    }
+
+    toggleChange(event) {
 
     }
-//Update du input au changement de sa valeur
-    toggleChange(e) {
-        this.setState({
-            inputValue : e.target.value
-        },() => console.log(this.state.inputValue))
-
-    };
-    addTask(e){
-        e.preventDefault();
-        this.setState({
-            inputValue : '',
-            tasks :[this.state.tasks, this.state.inputValue]
-        }, ()=> console.log(this.state))
-    };
 
     render() {
         return (
-            <Container>
-                 <Box
-                 display="flex"
-
-                 color="red"
-                 height={70}
-                 shape="rounded"
-                 alignItems="center"
-                 justifyContent="center"
-
-                 >
-                    <Heading color="white">
-                        Todo List - Ricardo
-                    </Heading>
-                </Box>
-                <Box
-                display="flex"
-                marginTop={12}
-                height={70}
-                shape="rounded"
-                alignItems="center"
-                justifyContent="center"
-                color="blue"
-                //shape="roundedBottom"
-                >
-                <form onSubmit={this.handleSubmit}>
-                    <input
-                        id="0"
-                        type="text"
-                        value={this.state.inputValue} onChange={this.toggleChange}
-                        />
-                    <button
-                        type="button"
-                        onClick={this.addTask}
-                        >
-                        AJOUTER
-                    </button>
-                </form>
-                        {
-                            this.state.tasks.map((task, index) => {
-                                return (
-                                    <p key={index}>
-                                        {task}| <button>x</button>
-                                    </p>
-                                )
-                            })
-                        }
-
-                </Box>
-            </Container>
+            <div>
+                <input type="text" value={this.state.inputValue} onChange={this.toggleChange}/>
+                <button type="button" onClick={this.addTask}>Add task</button>
+                {
+                    this.state.tasks.map((task, index) => {
+                        return (
+                            <p>{task}</p>
+                        )
+                    })
+                }
+            </div>
         );
     }
 }
